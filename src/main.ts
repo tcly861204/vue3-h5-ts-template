e6fd9e772a2c37c115b34d3a7bcce73e9923fc3f
tcly861204 <tcly861204@hotmail.com>
2025-04-19 17:49:47 +0800

    feat: ✨ 完善前端

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import router from "@/router/index"
 import store from "@/store/index"
+import "normalize.css/normalize.css"
 import "@/styles/global.css"
 const app = createApp(App)
 app.use(router)
